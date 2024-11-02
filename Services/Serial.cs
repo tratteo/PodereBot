c7554233eda681b7265724c9cec44801278ec0ba
tratteo <bltmtt@gmail.com>
2024-11-02 01:27:49 +0100

    Update Serial.cs

--- Services/Serial.cs
+++ Services/Serial.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.IO.Ports;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System.IO.Ports;
 
 namespace PodereBot.Services;
 
 internal class Serial
 {
-
     private const int BAUD_RATE = 115200;
 
     private readonly ILogger logger;
@@ -15,21 +14,16 @@ internal class Serial
     public Serial(ILogger<Serial> logger, IConfiguration configuration)
     {
         this.logger = logger;
-        var serials = SerialPort.GetPortNames();
         string? port = configuration.GetValue<string>("Serial:Port")!;
-        if (serials.Length <= 0)
-        {
-            logger.LogWarning("no available serial devices");
-        }
-        else if (!serials.Contains(port))
-        {
-            logger.LogWarning("no serial device found on {p}", port);
-        }
-        else
+        try
         {
             serialPort = new SerialPort(port, BAUD_RATE);
             serialPort.Open();
             logger.LogInformation("serial port ({p}) opened", serialPort.PortName);
+        }
+        catch (Exception ex)
+        {
+            logger.LogWarning("error opening serial bus: {ex}", ex);
         }
     }
 
